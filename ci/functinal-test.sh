0e3429479145d9565bb2517a2279c64ba377c305
Gibbus2 <gibbusf@gmail.com>
2024-08-16 14:30:29 +0200

    test for timeout and refused

--- ci/functinal-test.sh
+++ ci/functinal-test.sh
@@ -2,7 +2,18 @@ echo $1 | base64 -d > kubeconfig
 kubectl --kubeconfig kubeconfig port-forward service/service-frontend 8080 8080
 response=$(curl --write-out "%{http_code}" --silent --output /dev/null localhost:8080)
 echo "HTTP response code: $response"
+status=$?
+
+echo "HTTP response code: $response"
+echo "cURL exit status: $status"
+
 if [ "$response" -eq 404 ]; then
     echo "Resource not found (404). Exiting."
+    exit 1
+elif [ "$status" -eq 7 ]; then
+    echo "Connection refused (cURL error 7). Exiting."
+    exit 1
+elif [ "$status" -eq 28 ]; then
+    echo "Operation timeout (cURL error 28). Exiting."
     exit 1
 fi
