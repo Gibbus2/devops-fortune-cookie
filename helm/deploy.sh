1e8589708f8c5dfea86a2f2abb663e41e3ada564
CKSTools <caspa21@student.sdu.dk>
2024-08-16 16:56:43 +0200

    added sleep to ensure redis is deployed and running before backend

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -3,3 +3,5 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade --install redis bitnami/redis -f ./helm/redis.yaml --kubeconfig kubeconfig
 rm kubeconfig
+
+sleep 30
