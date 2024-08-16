68c7547ea58e13e5c621f00355e4ba367ff7d86f
Gibbus2 <gibbusf@gmail.com>
2024-08-16 15:32:34 +0200

    allow retries of curl

--- ci/functinal-test.sh
+++ ci/functinal-test.sh
@@ -1,15 +1,25 @@
+#!/bin/bash
 echo $1 | base64 -d > kubeconfig
-kubectl --kubeconfig kubeconfig port-forward svc/service-frontend 8080:8080 &
-sleep 5
-response=$(curl --write-out "%{http_code}" --silent --output /dev/null localhost:8080)
-echo "HTTP response code: $response"
-status=$?
+nohup kubectl --kubeconfig kubeconfig port-forward svc/service-frontend 8080:8080 &
+KUBECTL_PID=$!
 
-echo "HTTP response code: $response"
-echo "cURL exit status: $status"
+response=000
+retry_count=0
+max_retries=60
+
+while [ "$response" -eq 000 ] && [ $retry_count -lt $max_retries ]; do
+    retry_count=$((retry_count + 1))
+    response=$(curl --write-out "%{http_code}" --silent --output /dev/null localhost:8080)
+    echo "HTTP response code: $response"
+    status=$?
+    sleep 1
+done
 
 if [ "$response" -eq 404 ]; then
     echo "Resource not found (404). Exiting."
+    exit 1
+elif [ "$response" -eq 000 ]; then
+    echo "connection refused (000). Exiting."
     exit 1
 elif [ "$status" -eq 7 ]; then
     echo "Connection refused (cURL error 7). Exiting."
@@ -17,7 +27,9 @@ elif [ "$status" -eq 7 ]; then
 elif [ "$status" -eq 28 ]; then
     echo "Operation timeout (cURL error 28). Exiting."
     exit 1
-elif [ "$status" -eq 000 ]; then
-    echo "Unknwon return"
-    exit 1
 fi
+
+echo "HTTP response code: $response"
+echo "cURL exit status: $status"
+
+kill $KUBECTL_PID
